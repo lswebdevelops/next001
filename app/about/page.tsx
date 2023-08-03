9f78eba5f0d1fbcb9b6fd6ea8f3f340d808a69e6
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-02 22:12:30 -0300

    throw new Error("Not Today") on about page for testing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function About() {
+  throw new Error("Not Today")
+
   return (
     <>
     <h1>About</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from './page.module.css';
+
 
 export default function Home() {
   return (
-    <main>
+    <main 
+    // className={styles.main}
+    >
       <h1>Hello World!</h1>
       <Link href="/about">Go to About Page</Link>
     </main>
